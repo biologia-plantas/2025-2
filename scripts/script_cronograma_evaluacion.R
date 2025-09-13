6919e9685a09534c5b6c6e6dceebc5d0d3c1b45b
alexespinosaco <alexespinosacorrea@gmail.com>
2025-09-12 22:19:11 -0500

    Agegado cronograma de Medellín

--- scripts/script_cronograma_evaluacion.R
+++ scripts/script_cronograma_evaluacion.R
@@ -36,7 +36,7 @@ tbl_cronograma_oriente <-
     locations = gt::cells_column_labels()
   ) |> 
   gt::tab_footnote(
-    footnote = ("El cronograma puede variar dependiendo del desarrollo del curso"),
+    footnote = ("El cronograma puede variar dependiendo del desarrollo del curso")
   ) |> 
   gt::cols_width(
     fecha ~ gt::px(110)
@@ -74,6 +74,77 @@ tbl_evaluacion_general <-
   )
 
 ## Oriente evaluación laboratorio ---- 
+
+evaluacion_laboratorio <- 
+  googlesheets4::read_sheet(
+    "https://docs.google.com/spreadsheets/d/1HbEA1sPWc1Us1aAOGgMMlCMN19GKnS4tmQm0Aio0ZtA",
+    sheet = 3
+  )
+
+tbl_evaluacion_laboratorio <- 
+  evaluacion_laboratorio |> 
+  gt::gt() |> 
+  gt::sub_missing(
+    missing_text = ""
+  ) |> 
+  gt::cols_label(
+    criterio = "Criterio",
+    temas = "Temas",
+    porcentaje = "Porcentaje"
+  ) |> 
+  gt::fmt_percent(
+    columns = porcentaje,
+    decimals = 1
+  ) |> 
+  gt::opt_table_font(
+    font = gt::google_font(name = "Atkinson Hyperlegible")
+  ) |> 
+  gt::tab_style(
+    style = gt::cell_text(weight = "bold"),
+    locations = gt::cells_column_labels()
+  ) |> 
+  gt::tab_footnote(
+    footnote = ("Estos porcentajes corresponden al 25.0% del laboratorio"),
+  )
+
+## Medellín cronograma ----
+
+cronograma_medellin <- 
+  googlesheets4::read_sheet(
+    "https://docs.google.com/spreadsheets/d/1HbEA1sPWc1Us1aAOGgMMlCMN19GKnS4tmQm0Aio0ZtA",
+    sheet = 5
+  )
+
+tbl_cronograma_medellin <- 
+  cronograma_medellin |> 
+  gt::gt() |> 
+  gt::sub_missing(
+    missing_text = ""
+  ) |> 
+  gt::cols_label(
+    clase = "Clase",
+    miercoles = "Miércoles",
+    jueves = "Jueves",
+    laboratorio = "Laboratorio"
+  ) |> 
+  gt::fmt_markdown(
+    columns = c(laboratorio)
+  ) |> 
+  gt::opt_table_font(
+    font = gt::google_font(name = "Atkinson Hyperlegible")
+  ) |> 
+  gt::tab_style(
+    style = gt::cell_text(weight = "bold"),
+    locations = gt::cells_column_labels()
+  ) |> 
+  gt::tab_footnote(
+    footnote = ("El cronograma puede variar dependiendo del desarrollo del curso")
+  ) |> 
+  gt::cols_width(
+    c("miercoles", "jueves") ~ gt::px(110)
+  )
+
+## Medellín evaluación laboratorio ---- 
 
 evaluacion_laboratorio <- 
   googlesheets4::read_sheet(
